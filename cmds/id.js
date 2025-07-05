37ffb7e72577684b2b2b54236b6f5ef437c2d95c
Luxxy-dxd <dxdluffy5@gmail.com>
2025-07-05 23:17:30 +0545

    Update id.js

--- cmds/id.js
+++ cmds/id.js
@@ -1,23 +1,27 @@
 module.exports = {
-  name: "id",
-  usePrefix: false,
-  usage: "id [@mention]",
-  version: "1.0",
-  admin: false,
-  cooldown: 10,
+    name: "id",
+    usePrefix: false,
+    usage: "id [@mention]",
+    version: "1.0",
+    description: "Fetch the Facebook User ID (UID) of a mentioned user or yourself.",
+    admin: false,
+    cooldown: 10,
 
-  execute: async ({ api, event, mentions }) => {
-    const { threadID, messageID, senderID } = event;
+    execute: async ({ api, event }) => {
+        const { threadID, messageID, senderID, mentions } = event;
+        
+        let uid;
+        let userName;
 
-    let uid, name;
-    if (Object.keys(mentions).length > 0) {
-      uid = Object.keys(mentions)[0];
-      name = mentions[uid].replace("@", "") || "User";
-    } else {
-      uid = senderID;
-      name = "You";
+        // Check if a user is mentioned
+        if (Object.keys(mentions).length > 0) {
+            uid = Object.keys(mentions)[0]; // Get the first mentioned user's ID
+            userName = mentions[uid].replace("@", ""); // Get mentioned user's name
+        } else {
+            uid = senderID; // If no mention, get the sender's UID
+            userName = "You";
+        }
+        
+        api.sendMessage(`🔍 Facebook UID for ${userName}: ${uid}`, threadID, messageID);
     }
-
-    api.sendMessage(`🔍 Facebook UID for ${name}: ${uid}`, threadID, messageID);
-  }
 };
